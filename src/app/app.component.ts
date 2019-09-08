a153bdcbc7c1bce4d13394e9b3d68f64f967b79b
Janusz Cimek <info@jcimek.co.uk>
2019-09-08 14:45:10 +0200

    configure store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Store, select } from "@ngrx/store";
+// import { State } from "./reducers";
+// import { LoadActionss } from "./reducers/actions.actions";
+import { increment, decrement, reset } from "./store/counter/actions";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'storebase2';
+  count$: Observable<number>;
+
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.pipe(select("count"));
+  }
+
+  increase() {
+    this.store.dispatch(increment());
+  }
+  decrease() {
+    this.store.dispatch(decrement());
+  }
+  reset() {
+    this.store.dispatch(reset());
+  }
 }
